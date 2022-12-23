eb4cd39a0bb78f0aedb44d4c3891168e00d38f71
Andrew Kim <kim.andrew0121@gmail.com>
2022-12-22 21:54:27 -0800

    hw-3-final

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # password-generator
+
+## Description
+
+In this project, I coded a function to generate a random password based on character parameters. The function includes if statements, a while loop, arrays, and an event listener.
+
+## Installation
+
+N/A
+
+## Usage
+
+To use, open the webpage in the browser, then press the generate password button to generate a password. Select a password length of 8-128 characters. 4 confirm screens will display to choose character sets to include. After, a password will populate into the text box.
+
+![plot](./assets/screenshot-01.png)
+![plot](./assets/screenshot-02.png)
+![plot](./assets/screenshot-03.png)
+
+## Credits
+N/A
+
+## License
+
+PLease refer to the license in the repo
--- script.js
+++ script.js
@@ -8,55 +8,60 @@ var characters = [".", ",", "?", "!", "/", "-", "_", "+", "=", "@", "#", "$", "%
 
 // Write password to the #password input
 function writePassword() {
-    function generatePassword() {
-      // Ask user for password length
-      var pwLengthInput = prompt("Please enter password length (numbers only)");
-      //Converts user entry to a number
-      var pwLength = number(pwLengthInput);
-      //Confirm password is 8-128 characters
-      if (pwLength >= 8 && pwLength <= 128) {
-        //Ask user to confirm which characters to include in password
-        var lowerCaseInclude = confirm("Include lower case letters?");
-        var upperCaseInclude = confirm("Include upper case letters?");
-        var numberInclude = confirm("Include numbers?");
-        var characterInclude = confirm("Include special characters?");
-        //While loop selects a random character one at a time from selected arrays until password is correct length
-        var integer = 0;
-        while (password.length < pwLength) {
-          if(integer%4 == 0) {
-            if (lowerCaseInclude){
-              password = password.concat(lowerCase[math.floor(math.random()*lowerCase.length)]);
-            }
-          }
-          if(integer%4 == 1) {
-            if (upperCaseInclude){
-              password = password.concat(upperCase[math.floor(math.random()*upperCase.length)]);
-            }
-          }
-          if(integer%4 == 2) {
-            if (numberInclude){
-              password = password.concat(numbers[math.floor(math.random()*numbers.length)]);
-            }
-          }
-          if(integer%4 == 3) {
-            if (characterInclude){
-              password = password.concat(characters[math.floor(math.random()*characters.length)]);
-            }
-          }
-          integer++;
-        }
-
-      } else {
-        //Alerts user that password is too long, too short, or invalid
-        alert ("Invalid password.");
-        return;
-      }
-    };
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+};
+function generatePassword() {
+  // Ask user for password length and converts to an integer
+  var pwLengthInput = prompt("Please enter password length (numbers only)");
+  var pwLength = Number(pwLengthInput);
+  //Confirm password is 8-128 characters
+  if (pwLength >= 8 && pwLength <= 128) {
+    //Ask user to confirm which characters to include in password
+    var lowerCaseInclude = confirm("Include lower case letters?");
+    var upperCaseInclude = confirm("Include upper case letters?");
+    var numberInclude = confirm("Include numbers?");
+    var characterInclude = confirm("Include special characters?");
+    //Alerts user if no character selection is made
+    if (!lowerCaseInclude && !upperCaseInclude && !numberInclude && !characterInclude) {
+      alert ("Invalid response. Please include a character set.");
+      return "";
+    }
+    //While loop selects a random character one at a time from selected arrays until password is correct length
+    var integer = 0;
+    var password = "";
+    while (password.length < pwLength) {
+      if (integer%4 == 0) {
+        if (lowerCaseInclude){
+          password = password.concat(lowerCase[Math.floor(Math.random()*lowerCase.length)]);
+        }
+      }
+      if (integer%4 == 1) {
+        if (upperCaseInclude){
+          password = password.concat(upperCase[Math.floor(Math.random()*upperCase.length)]);
+        }
+      }
+      if (integer%4 == 2) {
+        if (numberInclude){
+          password = password.concat(numbers[Math.floor(Math.random()*numbers.length)]);
+        }
+      }
+      if (integer%4 == 3) {
+        if (characterInclude){
+          password = password.concat(characters[Math.floor(Math.random()*characters.length)]);
+        } 
+      }
+      integer++;
+    }
+    return password;
+  } else {
+    //Alerts user that password is too long, too short, or invalid
+    alert ("Invalid password.");
+    return "";
+  }
 };
 
 // Add event listener to generate button
